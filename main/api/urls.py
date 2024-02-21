45def04cc6f03a1ea9102f6e79cb5d8a32459ec5
Rustam Rustamov <20.06RustamRustamov@gmail.com>
2024-02-22 00:00:01 +0500

    added app_name

--- main/api/urls.py
+++ main/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import hello_world, hotel_list
 
+app_name = 'main'
+
 urlpatterns = [
   path('hello/', hello_world, name="greating"),
   path('hotels/', hotel_list, name="hotels"),
