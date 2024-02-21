ddd004429615c58e032d19a06d56b1c76c0647fc
Rustam Rustamov <20.06RustamRustamov@gmail.com>
2024-02-22 00:09:48 +0500

    added namespace to main urls

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('main.api.urls'), namespace="main"),
+    path('', include('main.api.urls', namespace="main")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
